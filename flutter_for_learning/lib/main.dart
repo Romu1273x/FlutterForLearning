3d6e1f9e33018d9ef8046d73562a151a944b1e7e
Romu1273x <romu1273x@yahoo.co.jp>
2021-11-06 20:09:29 +0900

    Flutter構成メモ
    Fultterの基本構成について調べたことをコメントにて記載。

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -4,23 +4,30 @@ void main() {
   runApp(const MyApp());
 }
 
+// StatelessWidget：状態を持たないWidget
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // MaterialApp：Flutterアプリケーションの全体を管理
+    // 主に全体のデザインや画面遷移をする場合の状態監視、アプリケーション全体のタイトルや全体にかかわるプロパティの管理
     return MaterialApp();
   }
 }
 
+// StatefulWidget：状態を持つWidget
 class MyHomePage extends StatefulWidget {
   @override
+  // StatefulWidgetのcreateStateを呼び出し、Stateを作成
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  // 描画したいWidgetを記述
   Widget build(BuildContext context) {
+    // Scaffold：モバイルアプリ画面を作る上でよく使われるメニュー表示方法を含めて画面全体を管理する部品
     return Scaffold();
   }
 }
