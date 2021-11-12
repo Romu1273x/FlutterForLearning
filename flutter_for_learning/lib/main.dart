f49e3ca594f42bd10957a05dfbc83c36522e02b9
Romu1273x <romu1273x@yahoo.co.jp>
2021-11-13 01:38:08 +0900

    アプリタイトルを表示

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -10,14 +10,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'タイトル';
+
     // MaterialApp：Flutterアプリケーションの全体を管理
     // 主に全体のデザインや画面遷移をする場合の状態監視、アプリケーション全体のタイトルや全体にかかわるプロパティの管理
-    return MaterialApp();
+    return MaterialApp(
+      title: appTitle,
+      // ThemeData：テーマを作成
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // ホーム画面にMyHomePageというWidgetを表示
+      home: const MyHomePage(title: appTitle),
+    );
   }
 }
 
 // StatefulWidget：状態を持つWidget
 class MyHomePage extends StatefulWidget {
+  //  titleは引数として受け取っている
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
   @override
   // StatefulWidgetのcreateStateを呼び出し、Stateを作成
   State<MyHomePage> createState() => _MyHomePageState();
@@ -28,6 +42,11 @@ class _MyHomePageState extends State<MyHomePage> {
   // 描画したいWidgetを記述
   Widget build(BuildContext context) {
     // Scaffold：モバイルアプリ画面を作る上でよく使われるメニュー表示方法を含めて画面全体を管理する部品
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // titleは親オブジェクトを継承して受け取っている
+        title: Text(widget.title),
+      ),
+    );
   }
 }
