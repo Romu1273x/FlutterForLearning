3fdd6746c889160d0699ba10b885426b40cff6cc
Romu1273x <romu1273x@yahoo.co.jp>
2022-01-12 23:10:00 +0900

    雛形

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -29,6 +29,39 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Text('リスト一覧画面'),
       ),
+      // +ボタンを表示
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context){
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+        ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // テキストボタンを表示
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
     );
   }
 }
