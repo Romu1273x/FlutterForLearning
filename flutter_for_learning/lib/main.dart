1b73dd27d23955d2ae6b277bf5bf6c4a1c0d912c
Romu1273x <romu1273x@yahoo.co.jp>
2022-02-05 23:38:24 +0900

    providerの使用

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_learning/chats.dart';
 import 'package:flutter_for_learning/screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  // MyApp()配下でChatDataを監視
+  runApp(ChangeNotifierProvider<ChatData>(
+    create: (context) => ChatData(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
