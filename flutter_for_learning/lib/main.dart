1ba8079710309862ba1d67a1538bea34c54d7656
Romu1273x <romu1273x@yahoo.co.jp>
2022-05-28 18:17:40 +0900

    ボトムナビゲーションのサンプル

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'home_page.dart';
+import 'navigation_util.dart';
+
+const androidColor = Color(0xFFFF80A0);
+const backpackColor = Color(0xFF66C3E3);
+const cakeColor = Color(0xFFE3DA4F);
 
 void main() {
   runApp(const MyApp());
@@ -6,24 +14,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PersistentBNB',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          unselectedItemColor: Colors.grey[300],
+        ),
       ),
-      home: const MyHomePage(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("ja", "JP"),
+      ],
+      home: WillPopScope(
+        onWillPop: () async {
+          // Android や iOS のスワイプジェスチャーでアプリが終了しないようにする
+          NavigationUtil.instance.popUntilHome();
+          return false;
+        },
+        child: const HomePage(),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
