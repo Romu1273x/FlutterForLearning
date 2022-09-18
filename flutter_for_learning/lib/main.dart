1787b64c6c1098dad76ae03767e5109d6d2f680a
Romu1273x <romu1273x@yahoo.co.jp>
2022-09-18 23:01:02 +0900

    コピペ
    https://qiita.com/atm_33/items/0b066c34280b39628910

--- flutter_for_learning/lib/main.dart
+++ flutter_for_learning/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_learning/chat_room.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const ChatRoom(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
